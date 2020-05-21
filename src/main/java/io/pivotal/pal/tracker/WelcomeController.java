77f7ed948589c6d0e71dd842a9079a64e04c1b5a
IDuncan23 <duncan.isaiah@gmail.com>
2020-05-21 14:49:56 -0500

    Change space Github secret, testing pipeline

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     String message;
+    String message2;
 
     public WelcomeController(@Value("${welcome.message}") String message){
         this.message = message;
